798428b70e1be242e0b4cdee3c03363d6cd15d17
Sam Elkhateeb <same@nanosn.com>
2011-11-17 22:36:03 -0700

    documented all methods

--- eventdb.js
+++ eventdb.js
@@ -1,28 +1,47 @@
 /**
- * @constructor
+ * @param {window.indexedDB} indexedDB HTML5 indexedDB implementation object
+ * @constructor eventdb
  */
 var eventdb = function(indexedDB) {
 	eventdb.indexedDB = indexedDB;
 };
 
 /**
- * name of database
+ * @type {string} name of database
+ * NOTE: for some reason if DBNAME = 'eventdb' nothing works! 
+ * 		 (tested on chrome only)
  */
 eventdb.DBNAME = 'edb';
 
 /**
- * version of database
+ * @type {number} version of database automatically adjusted 
+ * TODO: not sure if indexedDB version allows for number type but works on
+ * 		 chrome for now
  */
 eventdb.DBVERSION = 0.0;
 
+/**
+ * @type {string} object store name that holds 'eventdb.event' data
+ */
 eventdb.OBJECT_STORE_NAME = 'event';
 
+/**
+ * @type {window.IDBDatabase} holds the database instance once opened
+ */
 eventdb.prototype.db = null;
 
+/**
+ * general error handler
+ * @param {Object} e event
+ */
 eventdb.prototype.onerror = function(e) {
 	console.log("global error:", e);
 };
 
+/**
+ * opens the eventdb and inits the object store
+ * @param {function} onsuccess success callback 
+ */
 eventdb.prototype.open = function(onsuccess) {
 	var request = eventdb.indexedDB.open(eventdb.DBNAME);
 	var THIS = this;
@@ -32,7 +51,7 @@ eventdb.prototype.open = function(onsuccess) {
 		var db = THIS.db;
 		// We can only create Object stores in a setVersion transaction;
 		if(eventdb.DBVERSION !== db.version) {
-			THIS.createObjectStore(eventdb.OBJECT_STORE_NAME, {keyPath: "when"}, onsuccess);
+			THIS.createObjectStore_(eventdb.OBJECT_STORE_NAME, {keyPath: "when"}, onsuccess);
 		} else {
 			THIS.getAllEvents();
 		}
@@ -40,7 +59,15 @@ eventdb.prototype.open = function(onsuccess) {
 	};
 	request.onerror = this.onerror;
 };
-eventdb.prototype.createObjectStore = function(name, options, callback){
+
+/**
+ * creates the named object store
+ * @param {string} name name of object store to be created
+ * @param {object} options object store options
+ * @param {function} callback called when object store created successfully
+ * @private
+ */
+eventdb.prototype.createObjectStore_ = function(name, options, callback){
 	console.debug("createOS");
 	var THIS = this;
 	eventdb.DBVERSION += 0.1;
@@ -60,6 +87,10 @@ eventdb.prototype.createObjectStore = function(name, options, callback){
 		callback();
 	};
 };
+
+/**
+ * deletes the eventdb databese
+ */
 eventdb.prototype.deletedb = function() {
 	if(!eventdb.indexedDB.deleteDatabase){
 		console.warn("indexedDB.deleteDatebase not supported yet");
@@ -74,6 +105,13 @@ eventdb.prototype.deletedb = function() {
 	}
 };
 
+
+/**
+ * adds an event to object store
+ * @param {eventdb.event} event event object
+ * @param {function} onsuccess success callback
+ * @param {function} onerror error callback
+ */
 eventdb.prototype.addEvent = function(event, onsuccess, onerror) {
 	var db = this.db;
 	var trans = db.transaction([eventdb.OBJECT_STORE_NAME], IDBTransaction.READ_WRITE);
@@ -86,6 +124,10 @@ eventdb.prototype.addEvent = function(event, onsuccess, onerror) {
 	request.onerror = onerror;
 };
 
+/**
+ * deletes the event by id
+ * @param {string|number} id event id/key
+ */
 eventdb.prototype.deleteEvent = function(id) {
 	var db = this.db;
 	var trans = db.transaction([eventdb.OBJECT_STORE_NAME], IDBTransaction.READ_WRITE);
@@ -101,6 +143,10 @@ eventdb.prototype.deleteEvent = function(id) {
 	};
 };
 
+/**
+ * gets all events from database
+ * @param {function} onevent fires on every result record
+ */
 eventdb.prototype.getAllEvents = function(onevent) {
 	var db = this.db;
 	var trans = db.transaction([eventdb.OBJECT_STORE_NAME], IDBTransaction.READ_WRITE);
@@ -122,6 +168,11 @@ eventdb.prototype.getAllEvents = function(onevent) {
 	cursorRequest.onerror = this.onerror;
 };
 
+/**
+ * gets all objects from database
+ * @param {string} storeName the name of the store to get all data 
+ * @param {function} onrecord fires on every result record
+ */
 eventdb.prototype.getAll = function(storeName, onrecord) {
 	var trans = this.db.transaction([storeName], IDBTransaction.READ_ONLY);
 	var store = trans.objectStore(storeName);
@@ -132,7 +183,7 @@ eventdb.prototype.getAll = function(storeName, onrecord) {
 
 	cursorRequest.onsuccess = function(e) {
 		var result = e.target.result;
-		if(!!result == false)
+		if(!result)
 			return;
 
 		console.log(result.value);
@@ -155,6 +206,7 @@ eventdb.event.who = null;
 eventdb.event.what = null;//action, changed_from, changed_to
 eventdb.event.where = null;//url, browser, mobile
 eventdb.event.how = null;//how it happened? user clicked
+
 
 var edb = null;
 function init() {
